b51621e723037bcfc53fb41c025725655e5fe6d4
Joe Zales <joseph.q.zales@gmail.com>
2022-12-01 18:22:19 -0800

    Enables, decoders and frequency

--- Lattice/latticeE155FinalProject/impl_1/latticeE155FinalProject_impl_1_synthesize.tcl
+++ Lattice/latticeE155FinalProject/impl_1/latticeE155FinalProject_impl_1_synthesize.tcl
@@ -5,13 +5,13 @@ source [file join {C:/lscc/radiant/3.2} scripts tcl flow run_engine.tcl]
 # define global variables
 global para
 set para(gui_mode) 1
-set para(prj_dir) "C:/Users/afascetti/Desktop/MicroPsFinalProject/E155FinalProject/Lattice/latticeE155FinalProject"
+set para(prj_dir) "C:/Users/jzales/Desktop/E155FinalProject/E155FinalProject/Lattice/latticeE155FinalProject"
 # synthesize IPs
 # synthesize VMs
 # synthesize top design
 file delete -force -- latticeE155FinalProject_impl_1.vm latticeE155FinalProject_impl_1.ldc
 run_engine_newmsg synthesis -f "latticeE155FinalProject_impl_1_lattice.synproj"
-run_postsyn [list -a iCE40UP -p iCE40UP5K -t SG48 -sp High-Performance_1.2V -oc Industrial -top -w -o latticeE155FinalProject_impl_1_syn.udb latticeE155FinalProject_impl_1.vm] "C:/Users/afascetti/Desktop/MicroPsFinalProject/E155FinalProject/Lattice/latticeE155FinalProject/impl_1/latticeE155FinalProject_impl_1.ldc"
+run_postsyn [list -a iCE40UP -p iCE40UP5K -t SG48 -sp High-Performance_1.2V -oc Industrial -top -w -o latticeE155FinalProject_impl_1_syn.udb latticeE155FinalProject_impl_1.vm] "C:/Users/jzales/Desktop/E155FinalProject/E155FinalProject/Lattice/latticeE155FinalProject/impl_1/latticeE155FinalProject_impl_1.ldc"
 
 } out]} {
    runtime_log $out
--- Lattice/latticeE155FinalProject/source/impl_1/main.sv
+++ Lattice/latticeE155FinalProject/source/impl_1/main.sv
@@ -6,43 +6,48 @@
 module top(input  logic clk, // for simulation purposes, delete and make an internal clock in the top module when done simulating
            input  logic sck, 
            input  logic sdi,
+		   input  logic ce,
            output logic pwm);
                     
-    logic [47:0] flattenedMCUout, newFlattenedMCUout;
-	logic [7:0] sd0, sd1, sd2, sd3, sd4, sd5;
-	logic [9:0] freq; // max frequency of 1023 with 10 bits
+    logic [39:0] flattenedMCUout, newFlattenedMCUout;
+	logic [3:0] tone0, tone1, tone2, tone3, durMCU0, durMCU1, durMCU2, durMCU3;
+	logic [7:0] repThreshold;
+	logic [9:0] freq0, freq1, freq2, freq3, dur0, dur1, dur2, dur3; // max frequency of 1023 with 10 bits
 	logic [9:0] dur; // CHANGE, just for now doing 600
 	logic song;
 	logic int_osc;
-	logic start;
+	logic start, makingMusic;
 	
-	logic[35:0] clockSpeed;
+	logic[127:0] clockSpeed;
 
 	// clockspeed is 2.4MHz
 	assign clockSpeed = 2400000000;
 
-	// // Internal high-speed oscillator (instantiates the 24 MHz clock)
-	// HSOSC #(.CLKHF_DIV(2'b01))
-	// 	hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b0), .CLKHF(int_osc));
+	 // Internal high-speed oscillator (instantiates the 24 MHz clock)
+	HSOSC #(.CLKHF_DIV(2'b01))
+		hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b0), .CLKHF(int_osc));
 	
-	assign int_osc = clk;
+	//assign int_osc = clk;
 	
 	// get signal data from MCU
     MCU_spi spi(sck, sdi, newFlattenedMCUout);
 	
-	always_ff @(posedge int_osc) begin
-		if (newFlattenedMCUout != flattenedMCUout) begin
-			start <= 1;
+	always_ff @(negedge ce) begin
+		if (!makingMusic) begin
 			flattenedMCUout <= newFlattenedMCUout;
-			end
-		else start <= 0;
+			start <= 1;
+		end
 	end
 	
 	// decoder for the spi data to get the specific signals
-	make_signals makingSignals(flattenedMCUout, sd0, sd1, sd2, sd3, sd4, sd5);
+	make_signals makingSignals(flattenedMCUout, tone0, tone1, tone2, tone3, durMCU0, durMCU1, durMCU2, durMCU3, repThreshold);
+	
+	allTonesToFreq aT2F(tone0, tone1, tone2, tone3, freq0, freq1, freq2, freq3);
+	
+	allDurMCU2Durs ad2ds(durMCU0, durMCU1, durMCU2, durMCU3, dur0, dur1, dur2, dur3);
 	
 	// FSM to create unique tune from signal data
-	tune makeMusic(int_osc, start, sd0, sd1, sd2, sd3, sd4, sd5, clockSpeed, song);
+	tune makeMusic(int_osc, start, freq0, freq1, freq2, freq3, dur0, dur1, dur2, dur3, repThreshold, clockSpeed, makingMusic, song);
 	
 	assign pwm = song;
     
@@ -55,13 +60,13 @@ endmodule
 /////////////////////////////////////////////
 module MCU_spi(input  logic sck, 
                input  logic sdi,
-               output logic [47:0] newFlattenedMCUout);
+               output logic [39:0] newFlattenedMCUout);
 
     // SPI mode is equivalent to cpol = 0, cpha = 0 since data is sampled on first edge and the first
     // edge is a rising edge (clock going from low in the idle state to high).
 
 	always_ff @(posedge sck) begin
-		newFlattenedMCUout = {newFlattenedMCUout[46:0], sdi};
+		newFlattenedMCUout = {newFlattenedMCUout[38:0], sdi};
 	end
     
 endmodule
@@ -69,22 +74,126 @@ endmodule
 //////
 //    module to unpack the spi data into the signal data variables
 //////
-module make_signals(input logic[47:0] flattenedMCUout,
-					output logic[7:0] sd0,
-					output logic[7:0] sd1,
-					output logic[7:0] sd2,
-					output logic[7:0] sd3,
-					output logic[7:0] sd4,
-					output logic[7:0] sd5);
+module make_signals(input logic[39:0] flattenedMCUout,
+					output logic[3:0] tone0,
+					output logic[3:0] tone1,
+					output logic[3:0] tone2,
+					output logic[3:0] tone3,
+					output logic[3:0] dur0,
+					output logic[3:0] dur1,
+					output logic[3:0] dur2,
+					output logic[3:0] dur3,
+					output logic[7:0] repThreshold);
 	
 	// match up signal variables to the crunched together version (variable 0 = msb)
-	assign sd0 = flattenedMCUout[47:40];
-	assign sd1 = flattenedMCUout[39:32];
-	assign sd2 = flattenedMCUout[31:24];
-	assign sd3 = flattenedMCUout[23:16];
-	assign sd4 = flattenedMCUout[15:8];
-	assign sd5 = flattenedMCUout[7:0];
+	assign tone0 = flattenedMCUout[39:36];
+	assign dur0  = flattenedMCUout[35:32];
+	assign tone1 = flattenedMCUout[31:28];
+	assign dur1  = flattenedMCUout[27:24];
+	assign tone2 = flattenedMCUout[23:20];
+	assign dur2  = flattenedMCUout[19:16];
+	assign tone3 = flattenedMCUout[15:12];
+	assign dur3  = flattenedMCUout[11:8];
+	assign repThreshold   = flattenedMCUout[7:0];
 	
+endmodule
+
+//////
+//    module to decode all tones into frequencies
+//////
+module allTonesToFreq(input logic[3:0] tone0,
+					input logic[3:0] tone1,
+					input logic[3:0] tone2,
+					input logic[3:0] tone3,
+					output logic[9:0] freq0,
+					output logic[9:0] freq1,
+					output logic[9:0] freq2,
+					output logic[9:0] freq3);
+					
+	toneToFreq t2F0(tone0, freq0);
+	toneToFreq t2F1(tone1, freq1);
+	toneToFreq t2F2(tone2, freq2);
+	toneToFreq t2F3(tone3, freq3);
+	
+endmodule
+
+
+//////
+//    module to decode tone into frequency
+//////
+module toneToFreq(input logic[3:0] tone,
+					output logic[9:0] freq);
+
+	always_comb begin
+		case(tone)
+			0	:	freq = 220;	// A3
+			1	:	freq = 247;	// B3
+			2	:	freq = 262;	// C4
+			3	: 	freq = 294;	// D4
+			4	:	freq = 330;	// E4
+			5	:	freq = 349;	// F4
+			6 	:	freq = 392; // G4
+			7	: 	freq = 440; // A4
+			8	:	freq = 494;	// B4
+			9	:	freq = 523;	// C5
+			10	:	freq = 587;	// D5
+			11	:	freq = 659;	// E5
+			12	:	freq = 698; // F5
+			13	:	freq = 784;	// G5
+			14	:	freq = 880;	// A5
+			15 	: 	freq = 262; // C4 (for the extra)
+			default : freq = 262; // C4 (default)
+		endcase
+	end			
+endmodule
+
+//////
+//    module to decode all durMCUs into durations (ms)
+//////
+module allDurMCU2Durs(input logic[3:0] durMCU0,
+					input logic[3:0] durMCU1,
+					input logic[3:0] durMCU2,
+					input logic[3:0] durMCU3,
+					output logic[9:0] dur0,
+					output logic[9:0] dur1,
+					output logic[9:0] dur2,
+					output logic[9:0] dur3);
+					
+	durMCUtoDuration d2d0(durMCU0, dur0);
+	durMCUtoDuration d2d1(durMCU1, dur1);
+	durMCUtoDuration d2d2(durMCU2, dur2);
+	durMCUtoDuration d2d3(durMCU3, dur3);
+	
+endmodule
+
+
+//////
+//    module to decode durD into duration in ms
+//////
+module durMCUtoDuration(input logic[3:0] durMCU,
+					output logic[9:0] dur);
+
+	always_comb begin
+		case(durMCU)
+			0	:	dur = 1000;	// whole note (1 second)
+			1	:	dur = 1000;	// whole note (1 second)
+			2	:	dur = 1000;	// whole note (1 second)
+			3	: 	dur = 1000;	// whole note (1 second)
+			4	:	dur = 1000;	// whole note (1 second)
+			5	:	dur = 500;	// half note  (0.5 seconds)
+			6 	:	dur = 500;  // half note  (0.5 seconds)
+			7	: 	dur = 500;  // half note  (0.5 seconds)
+			8	:	dur = 500;	// half note  (0.5 seconds)
+			9	:	dur = 500;	// half note  (0.5 seconds)
+			10	:	dur = 500;	// half note  (0.5 seconds)
+			11	:	dur = 250;	// quarter note (0.25 seconds)
+			12	:	dur = 250;  // quarter note (0.25 seconds)
+			13	:	dur = 250;	// quarter note (0.25 seconds)
+			14	:	dur = 250;	// quarter note (0.25 seconds)
+			15 	: 	dur = 250;  // quarter note (0.25 seconds)
+			default : dur = 250; // default: quarter note (0.25 seconds)
+		endcase
+	end			
 endmodule
 
 //////
@@ -93,13 +202,17 @@ endmodule
 //////
 module tune(input logic int_osc,
 				input logic start,
-				input logic[7:0] sd0, 
-				input logic[7:0] sd1,
-				input logic[7:0] sd2,
-				input logic[7:0] sd3,
-				input logic[7:0] sd4,
-				input logic[7:0] sd5,
-				input logic[35:0] clockSpeed,
+				input logic[9:0] freq0,
+				input logic[9:0] freq1,
+				input logic[9:0] freq2,
+				input logic[9:0] freq3,
+				input logic[9:0] dur0,
+				input logic[9:0] dur1,
+				input logic[9:0] dur2,
+				input logic[9:0] dur3,
+				input logic[7:0] repThreshold,
+				input logic[127:0] clockSpeed,
+				output logic makingMusic,
 				output logic song);
 				
 	logic done, en, rep;
@@ -107,22 +220,28 @@ module tune(input logic int_osc,
 	logic[7:0] dur;
 	logic[9:0] freq;
 	
-	// state and next state definitions
-	typedef enum logic[1:0] {idle, note1, note2, note3} statetype;
-	statetype state, nextstate;
-	// state register
-	always_ff @(posedge int_osc) begin
-		if(start) 	state <= note1;
-		else 		state <= nextstate;
-	end
-	
 	// assign repeat
-	assign threshold = 2;//((sd0 + sd5) % 3);        		// TODO - make a new signal to assign to how many times we repeat (mod on the MCU side)
+	assign threshold = repThreshold[1:0];        		// TODO - make a new signal to assign to how many times we repeat (mod on the MCU side)
 	assign rep = ~(counter == threshold);
 	
+		
 	// instantiate dur and freq modules
 	duration howLong(int_osc, dur, clockSpeed, done);
 	freqGenerator pitch(int_osc, en, freq, clockSpeed, toneFreq);
+	
+	// state and next state definitions
+	typedef enum logic[2:0] {idle, note0, note1, note2, note3, complete} statetype;
+	statetype state, nextstate;
+	
+	// state register
+	always_ff @(posedge int_osc) begin
+		state <= nextstate;
+	end
+	//always_ff @(posedge int_osc) begin
+	//	if(start) 	state <= note0;
+	//	else 		state <= nextstate;
+	//end
+	
 	
 	// only play music when we aren't in the idle state
 	always_ff @(posedge int_osc) begin
@@ -130,18 +249,31 @@ module tune(input logic int_osc,
 		else en <= 1;
 	end
 	
+	// if completed doorbell chime, flag makingMusic goes low
+	// if completed doorbell chime, flag makingMusic goes low
 	always_ff @(posedge int_osc) begin
-		if (state == note1) begin
-			freq <= (sd0*3);
-			dur <= sd1;
+		if (state == complete) makingMusic <= 0;
+		else if (start) makingMusic <= 1;
+	end
+	
+		
+	// freq gets the frequency/duration of that note if its in that note's state
+	always_ff @(posedge int_osc) begin
+		if (state == note0) begin
+			freq <= freq0;
+			dur <= dur0;
+		end
+		else if (state == note1) begin
+			freq <= freq1;
+			dur <= dur1;
 		end
 		else if (state == note2) begin
-			freq <= (sd2*3);
-			dur <= sd3;
+			freq <= freq2;
+			dur <= dur2;
 		end
 		else if (state == note3) begin
-			freq <= (sd4*3);
-			dur <= sd5;
+			freq <= freq3;
+			dur <= dur3;
 			counter <= counter + 1;
 		end
 	end
@@ -149,15 +281,19 @@ module tune(input logic int_osc,
 	// nextstate logic
 	always_comb
 		case(state)
-			note1: 	if(done)				nextstate = note2;
-					else					nextstate = note1;
-			note2: 	if(done)				nextstate = note3;
-					else					nextstate = note2;
-			note3: 	if(done && rep)			nextstate = note1;
-					else if (done)			nextstate = idle;
-					else					nextstate = note3;
-			idle:	if(start)				nextstate = note1;
-					else					nextstate = idle;
+			note0: 		if(done)				nextstate = note1;
+						else					nextstate = note0;
+			note1: 		if(done)				nextstate = note2;
+						else					nextstate = note1;
+			note2: 		if(done)				nextstate = note3;
+						else					nextstate = note2;
+			note3: 		if(done && rep)			nextstate = note0;
+						else if (done)			nextstate = complete;
+						else					nextstate = note3;
+			complete:							nextstate = idle;
+			idle:		if(start)				nextstate = note0;
+						else					nextstate = idle;
+			default: nextstate = idle;
 		endcase
 	
 	// output
@@ -166,11 +302,11 @@ module tune(input logic int_osc,
 endmodule
 
 //////
-//    takes in a unique duration value, outputs a flag when that duration has elapsed
+//    takes in a unique duration value in ms, outputs a flag when that duration has elapsed
 /////
 module duration(input logic int_osc,
 				input logic[7:0] dur,
-				input logic[35:0] clockSpeed,
+				input logic[127:0] clockSpeed,
 				output logic done);
 				
 	logic[31:0] counter = 0;
@@ -219,7 +355,7 @@ endmodule
 /////
 module freqGenerator(input logic int_osc, en,
 				input logic[9:0] freq,
-				input logic[35:0] clockSpeed,
+				input logic[127:0] clockSpeed,
 				output logic toneFreq);
 	logic clkStrobe;
 	logic[31:0] counter;
